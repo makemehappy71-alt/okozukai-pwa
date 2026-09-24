4718e5de6d262eebbcd6bdf19fcdbbad76916c01
makemehappy71-alt <make.me.happy71@gmail.com>
2026-09-24 18:02:22 +0900

    Cache receipt feature in V3.2.8.5.0

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE = "okozukai-v3.2.8.4.5-prod-1";
+const CACHE = "okozukai-v3.2.8.5.0-prod-1";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=32845",
-  "./app.js?v=32845",
+  "./styles.css?v=32850",
+  "./app.js?v=32850",
+  "./receipt.js?v=32850",
   "./manifest.webmanifest",
   "./icon.svg",
   "./icon-192.png",

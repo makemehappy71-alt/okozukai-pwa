3dc560c73f9c34486b81a39b3c17e25ba61eed88
makemehappy71-alt <make.me.happy71@gmail.com>
2026-09-25 12:25:59 +0900

    Refresh PWA cache for V3.2.8.5.8

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE = "okozukai-v3.2.8.5.7-prod-1";
+const CACHE = "okozukai-v3.2.8.5.8-prod-1";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=32857",
-  "./app.js?v=32857",
-  "./receipt.js?v=32857",
+  "./styles.css?v=32858",
+  "./app.js?v=32858",
+  "./receipt.js?v=32858",
   "./manifest.webmanifest",
   "./icon.svg",
   "./icon-192.png",
